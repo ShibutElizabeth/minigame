29e3d2126920e101498706c87e3ebedb1d541a22
ShibutElizabeth <ellis.shybut@mail.ru>
2024-10-20 22:07:02 +0800

    feat: mini crystals work

--- src/js/ProgressBars.js
+++ src/js/ProgressBars.js
@@ -9,44 +9,106 @@ export default class ProgressBars {
         this.columns = 5;
 
         this.loader = new Loader();
-        this.bars = null;
+        this.textures = null;
+        this.minicrystals = null;
 
-        this.progressBarSprites = []; // Список для хранения спрайтов прогресс-баров
+        this.sprites = []; // Список для хранения спрайтов прогресс-баров
+        this.minisprites = [];
         this.calculateSizes(this.width); // Вызываем при инициализации
 
         window.addEventListener('resize', this.onResize.bind(this));
     }
 
     // Асинхронная загрузка текстур прогресс-баров
-    async loadProgressBarsTextures() {
-        if (!this.bars) {
-            this.bars = await this.loader.loadProgressBars();
+    async loadTextures() {
+        if (!this.textures) {
+            this.textures = await this.loader.loadProgressBars();
+        }
+        if (!this.minicrystals) {
+            this.minicrystals = await this.loader.loadMiniCrystals();
         }
     }
 
     // Рассчитываем отступы и размеры элементов
     calculateSizes(width) {
-        this.margin = { top: 0.012 * width, left: 0.077 * width };
+        this.barMargin = { top: 0.012 * width, left: 0.077 * width };
         this.barSize = { width: 0.154 * width, height: 0.0668 * width };
+        this.miniMargin = {
+            left: 0.15 * this.width,
+            top: 0.0154 * this.width
+        };
+        this.miniPadding = {
+            left: 0.0075 * this.width,
+            between: 0.174 * this.width
+        };
+        this.miniSize = {
+            width: 0.0137 * this.width,
+            height: 0.0145 * this.width
+        }
     }
 
     // Размещаем прогресс-бары на сцене
-    placeProgressBars() {
+    place() {
         // Удаляем старые спрайты перед пересчетом позиций
-        this.progressBarSprites.forEach(sprite => this.app.stage.removeChild(sprite));
-        this.progressBarSprites = [];
+        this.clearSprites();
 
         // Размещаем новые спрайты
         for (let col = 0; col < this.columns; col++) {
-            const barSprite = new Sprite(this.bars[crystalTypes[col]]);
-            barSprite.x = this.margin.left + col * (this.barSize.width + 0.02 * this.width); // Добавим небольшой отступ между столбцами
-            barSprite.y = this.margin.top;
+            const barSprite = new Sprite(this.textures[crystalTypes[col]]);
+            barSprite.x = this.barMargin.left + col * (this.barSize.width + 0.02 * this.width); // Добавим небольшой отступ между столбцами
+            barSprite.y = this.barMargin.top;
             barSprite.width = this.barSize.width;
             barSprite.height = this.barSize.height;
 
-            this.progressBarSprites.push(barSprite);
+            this.sprites.push(barSprite);
             this.app.stage.addChild(barSprite);
         }
+        this.placeMinicrystals();
+    }
+
+    placeMinicrystals(){
+        const positions = [];
+
+        for(let type = 0; type < this.columns; type++){
+            for(let c = 0; c < 3; c++){
+                positions.push({
+                    x: this.miniMargin.left + c * (this.miniPadding.left + this.miniSize.width) + type * this.miniPadding.between,
+                    y: this.miniMargin.top,
+                    type: type
+                })
+            }
+        }
+
+        positions.forEach((el) => {
+            const sprite = new Sprite(this.minicrystals[crystalTypes[el.type]]);
+            sprite.x = el.x;
+            sprite.y = el.y;
+            sprite.width = this.miniSize.width;
+            sprite.height = this.miniSize.height;
+            sprite.visible = false;
+
+            this.minisprites.push(sprite);
+            this.app.stage.addChild(sprite);
+        })
+    }
+
+    // Метод для обновления видимости миникристаллов
+    updateMiniCrystals(progress) {
+        this.minisprites.forEach((sprite, index) => {
+            const crystalType = crystalTypes[Math.floor(index / 3)]; // Определяем тип кристалла
+            const crystalProgress = progress[crystalType];
+
+            // Делаем миникристалл видимым, если прогресс >= позиции кристалла (например, для первого миникристалла прогресс >= 1)
+            sprite.visible = crystalProgress > (index % 3);
+        });
+    }
+
+    clearSprites() {
+        // Удаляем старые спрайты прогресс-баров и миникристаллов
+        this.sprites.forEach(sprite => this.app.stage.removeChild(sprite));
+        this.minisprites.forEach(sprite => this.app.stage.removeChild(sprite));
+        this.sprites = [];
+        this.minisprites = [];
     }
 
     // Пересчитываем позиции и размеры на изменение окна
@@ -55,6 +117,10 @@ export default class ProgressBars {
         this.calculateSizes(this.width); // Пересчитываем размеры
 
         // Перерасполагаем прогресс-бары после ресайза
-        this.placeProgressBars();
+        this.place();
+    }
+
+    restart(){
+
     }
 }
