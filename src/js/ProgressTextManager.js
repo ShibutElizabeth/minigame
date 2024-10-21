bc9e210be27acb9c73740a091cb984b44e5abe0d
ShibutElizabeth <ellis.shybut@mail.ru>
2024-10-21 14:04:27 +0800

    feat: progress texts work

--- src/js/ProgressTextManager.js
+++ src/js/ProgressTextManager.js
@@ -5,15 +5,17 @@ export default class ProgressTextManager {
 
     // Обновление текста прогресса
     updateProgress(key, value) {
+        const inner = `Progress\n${value}/3`;
         if (this.progressTexts.has(key)) {
-            this.progressTexts.get(key).textContent = `Progress ${value}/3`;
+            this.progressTexts.get(key).text = inner.toUpperCase();
         }
     }
 
     // Сброс всех текстов
     resetProgress() {
+        const inner = `Progress\n0/3`
         for (const [key, textElement] of this.progressTexts.entries()) {
-            textElement.textContent = `Progress 0/3`;
+            textElement.text = inner;
         }
     }
 }
